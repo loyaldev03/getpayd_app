76803dbcbcf78a7a5bae866968d1a003546b0b3c
future21star <future21star@yahoo.com>
2017-12-09 07:48:50 -0500

    task completed model tweak

--- src/server/task_completed.model.js
+++ src/server/task_completed.model.js
@@ -10,3 +10,17 @@ var taskCompletedSchema = mongoose.Schema({
 var TaskCompleted = mongoose.model('TaskCompleted', taskCompletedSchema);
 
 module.exports = TaskCompleted;
+
+
+var mongoose = require('mongoose');
+
+var taskCompletedSchema = mongoose.Schema({
+    task: Object,
+    user: Object,
+    points_awarded: Number,
+    time: Date
+});
+
+var TaskCompleted = mongoose.model('TaskCompleted', taskCompletedSchema);
+
+module.exports = TaskCompleted;
