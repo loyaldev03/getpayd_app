4cf14142bba4c39a6461bd6476649657f7d54e20
future21star <future21star@yahoo.com>
2017-12-09 07:54:45 -0500

    authentication guard for redirecting user

--- src/app/_guards/auth_admin.guard.ts
+++ src/app/_guards/auth_admin.guard.ts
@@ -14,3 +14,19 @@ export class AuthAdminGuard implements CanActivate {
         return false;
     }
 }
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+
+@Injectable()
+export class AuthAdminGuard implements CanActivate {
+
+    constructor(private router: Router) { }
+
+    canActivate() {
+        if (localStorage.getItem('currentUser') && ((JSON.parse(localStorage.getItem('currentUser')).email === 'admin@gmail.com') || (JSON.parse(localStorage.getItem('currentUser')).company_name != null))) {
+            return true;
+        }
+        this.router.navigate(['/user/user']);
+        return false;
+    }
+}
