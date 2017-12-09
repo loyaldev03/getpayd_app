016f0ffac49d2571e8219c04b4d6a850667a634a
future21star <future21star@yahoo.com>
2017-12-09 07:49:45 -0500

    fix bug for points awarded model

--- src/server/points_awarded.model.js
+++ src/server/points_awarded.model.js
@@ -10,3 +10,17 @@ var pointsAwardedSchema = mongoose.Schema({
 var PointsAwarded = mongoose.model('PointsAwarded', pointsAwardedSchema);
 
 module.exports = PointsAwarded;
+
+
+var mongoose = require('mongoose');
+
+var pointsAwardedSchema = mongoose.Schema({
+    points_awarded: Number,
+    task_id: String,
+    user: Object,
+    time: Date
+});
+
+var PointsAwarded = mongoose.model('PointsAwarded', pointsAwardedSchema);
+
+module.exports = PointsAwarded;
