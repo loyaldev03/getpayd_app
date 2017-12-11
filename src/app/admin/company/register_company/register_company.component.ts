b613414c98a8e328f90903dcc0ddba7b94a08005
future21star <future21star@yahoo.com>
2017-12-10 22:02:10 -0500

    company registration optimization logic

--- src/app/admin/company/register_company/register_company.component.ts
+++ src/app/admin/company/register_company/register_company.component.ts
@@ -39,3 +39,45 @@ export class RegisterCompanyComponent implements OnInit{
         }
     }
 }
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { AlertService, UserService, AdminService } from '../../../_services/index';
+
+@Component({
+    templateUrl: 'register_company.component.html'
+})
+
+export class RegisterCompanyComponent implements OnInit{
+    private id: number = null;
+    model: any = {};
+    loading = false;
+
+    constructor(
+        private router: Router,
+        private route:ActivatedRoute,
+        private adminService: AdminService,
+        private alertService: AlertService) { }
+
+    ngOnInit(): void {
+        this.model._id = this.route.params['_value']['id'];
+    }
+
+    register() {
+        this.loading = true;
+        console.log("company id", this.model._id);
+        if (this.model._id){
+            this.adminService.editCompany(this.model)
+            .subscribe(
+                data => {
+                        this.alertService.success('Registration successful', true);
+                        this.router.navigate(['/login']);                    
+                    },
+                error => {
+                        this.alertService.error(error);
+                        this.loading = false;
+                });
+        }
+    }
+}
