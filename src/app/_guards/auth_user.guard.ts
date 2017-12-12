73ab1779b26d7be2bf208ed9f41bfbf48f6eecb6
future21star <future21star@yahoo.com>
2017-12-12 17:00:47 -0500

    add guards for authentication

--- src/app/_guards/auth_user.guard.ts
+++ src/app/_guards/auth_user.guard.ts
@@ -14,3 +14,21 @@ export class AuthUserGuard implements CanActivate {
         return false;
     }
 }
+
+
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+
+@Injectable()
+export class AuthUserGuard implements CanActivate {
+
+    constructor(private router: Router) { }
+
+    canActivate() {
+        if (localStorage.getItem('currentUser')) {
+            return true;
+        }
+        this.router.navigate(['/login']);
+        return false;
+    }
+}
