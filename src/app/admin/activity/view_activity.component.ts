7497f72352ab026dbb82e353c228066b93714943
future21star <future21star@yahoo.com>
2017-12-16 19:12:49 -0500

    remove activities for view

--- src/app/admin/activity/view_activity.component.ts
+++ src/app/admin/activity/view_activity.component.ts
@@ -54,3 +54,60 @@ export class ViewActivityComponent implements OnInit{
   }
 
 }
+
+import { Component, OnInit }        from '@angular/core';
+import { AdminService }     from '../../_services/admin.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Component, OnInit }        from '@angular/core';
+import { AdminService }     from '../../_services/admin.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+    templateUrl: 'view_activity.component.html',
+    styleUrls: [ './view_activity.component.scss' ]
+})
+export class ViewActivityComponent implements OnInit{
+  private activities = [];
+  private isLoading: boolean = true;
+  private search_str:string = "";
+  private user_id: string = "";
+  constructor(
+    private adminService: AdminService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.getActivities(params["user_id"]);
+    });
+  }
+
+  getActivities(user_id) {
+    if (user_id === 'all'){
+      this.adminService.getUsers().subscribe(
+        data => {
+          this.activities = [];
+          for (let index in data) {
+            this.activities = this.activities.concat(data[index].activities);
+          }        
+          console.log(this.activities);
+        },
+        error => console.log(error),
+        () => {this.isLoading = false}
+      );      
+    }
+    else {
+      this.adminService.getUser(user_id).subscribe(
+        data => {
+          this.activities = data.activities;
+          console.log(this.activities);
+        },
+        error => console.log(error),
+        () => {this.isLoading = false}
+      );            
+    }
+  }
+
+}
