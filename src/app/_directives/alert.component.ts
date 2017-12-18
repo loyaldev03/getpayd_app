3b2c0ca807963abeeb72f3d3737eed889bda1378
awhaskell <awhaskell>
2017-12-18 14:44:10 -0500

    customize for alerts controller

--- src/app/_directives/alert.component.ts
+++ src/app/_directives/alert.component.ts
@@ -43,3 +43,49 @@ export class AlertComponent {
         this.alertService.getMessage().subscribe(message => { this.message = message; });
     }
 }
+
+import { Component, OnInit } from '@angular/core';
+
+import { AlertService } from '../_services/index';
+import { AlertService } from '../_services/index';
+import { AlertService } from '../_services/index';
+import { AlertService } from '../_services/index';
+import { AlertService } from '../_services/index';
+
+@Component({
+    selector: 'alert',
+    templateUrl: 'alert.component.html'
+})
+
+export class AlertComponent {
+    message: any;
+
+    constructor(private alertService: AlertService) { }
+
+    ngOnInit() {
+        this.alertService.getMessage().subscribe(message => { this.message = message; });
+    }
+}
+
+import { Component, OnInit } from '@angular/core';
+
+import { AlertService } from '../_services/index';
+import { AlertService } from '../_services/index';
+import { AlertService } from '../_services/index';
+import { AlertService } from '../_services/index';
+import { AlertService } from '../_services/index';
+
+@Component({
+    selector: 'alert',
+    templateUrl: 'alert.component.html'
+})
+
+export class AlertComponent {
+    message: any;
+
+    constructor(private alertService: AlertService) { }
+
+    ngOnInit() {
+        this.alertService.getMessage().subscribe(message => { this.message = message; });
+    }
+}
